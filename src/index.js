a6d7ec0b4b67bef616cf2015a5b61ce190962c55
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2024-01-15 10:41:23 +0000

    Temperature and description display

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 
-function displayWeatherData (event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector(".temperature");
-    temperatureElement.innerHTML= "Hello from index.js";
+// function displayWeatherData (event) {
+//     event.preventDefault();
+//     let temperatureElement = document.querySelector("#temperature");
+//     temperatureElement.innerHTML= "Hello from index.js";
+    
+// }
 
+function callApi (response) {
+    console.log(response);
+      let temperatureElement = document.querySelector("#temperature");
+      temperatureElement.innerHTML = (Math.round(response.data.main.temp));
+      let descriptionElement = document.querySelector("#description");
+      descriptionElement.innerHTML = "Description: " + response.data.weather[0].description;
+      let celsiusElement=document.querySelector("#celsius");
+      celsiusElement.innerHTML = "℃";
 }
 
+function parseApi (city) {
+    let apiKey = "d1b6ead1e59b61fc5c228b89a0df9361";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(callApi);
+}
 
-let submit =document.querySelector(".submit");
-submit.addEventListener("click", displayWeatherData);
+function updateCity (event) {
+    event.preventDefault();
+    let userInput = document.querySelector("#search")
+    parseApi(userInput.value);
+}
+
+//OpenWeather API
+
+//City submit parse to API
+
+//onChange
+
+let submit =document.querySelector("#submit");
+submit.addEventListener("click", updateCity);
 
 
